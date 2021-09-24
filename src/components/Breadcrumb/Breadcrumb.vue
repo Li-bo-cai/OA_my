21488ebc4755ab6270869ab4edf4c26ed26faad1
LbcNewBee <1519497988@qq.com>
2021-09-24 22:46:06 +0800

    0924

--- src/components/Breadcrumb/Breadcrumb.vue
+++ src/components/Breadcrumb/Breadcrumb.vue
@@ -4,8 +4,11 @@
     <el-breadcrumb separator-class="el-icon-arrow-right" class="o-breadcrumb">
       <el-breadcrumb-item v-for="route in breadList" :key="route.path" separator="/" :to="route.path">{{ route.meta.title }}</el-breadcrumb-item>
     </el-breadcrumb>
-    <div class="back_to_home" @click="back_home">
+    <div v-if="titel=='backhome'" class="back_to_home" @click="back_home">
       <span>返回首页</span>
+    </div>
+    <div v-if="titel=='pageup'" class="back_to_home" @click="page_up">
+      <span>返回上一页</span>
     </div>
   </div>
 
@@ -15,6 +18,9 @@
 import { ref, defineComponent, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
 export default defineComponent({
+  props: {
+    title: String,
+  },
   setup() {
     const route = useRoute();
     const router = useRouter();
