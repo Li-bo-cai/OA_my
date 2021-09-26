8af538dad027f2daa577f540ce69c24681168a5e
LbcNewBee <1519497988@qq.com>
2021-09-26 22:58:18 +0800

    0926

---
+++ src/components/Pagination/Pagination.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="pagin_ation">
+    <el-pagination background :page-sizes="[100, 200, 300, 400]" :page-size="100" layout="total, prev, pager, next, sizes" :total="total" @size-change="handleSizeChange" @current-change="handleCurrentChange">
+    </el-pagination>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+    // .el-pagination{
+    //     background: rgb(243, 243, 243);
+    // }
+</style>
