2d7112d53cb3c2a28d3110528e7e336748ba449a
LbcNewBee <1519497988@qq.com>
2022-03-11 23:06:09 +0800

    demo

---
+++ src/views/Oa_approve/All/Demo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    子组件{{modelValue}}
+    <button @click='handleEdit'>点击</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    modelValue: {
+      type: String,
+      default: "",
+    },
+  },
+  setup(props, context) {
+    const handleEdit = () => {
+      console.log("我发出了");
+      context.emit("upload:modelValue", "nihao");
+    };
+    return {
+      handleEdit,
+    };
+  },
+});
+</script>
+
+<style scoped>
+</style>
--- src/views/Oa_approve/All/index.vue
+++ src/views/Oa_approve/All/index.vue
@@ -3,24 +3,29 @@
     <div class="header">全部</div>
     <el-button type="primary" @click="dialog= true">点击我展开弹窗</el-button>
     <PeopleChoseDialog :dialog="dialog" @closeDialog="myModealHide"></PeopleChoseDialog>
+    <Demo v-model="title"></Demo>
   </div>
 </template>
 
 <script lang="ts">
 import PeopleChoseDialog from "@/components/PeopleChoseDialog/index.vue";
+import Demo from "./Demo.vue";
 import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   components: {
     PeopleChoseDialog,
+    Demo,
   },
   setup() {
     const dialog = ref<boolean>(false);
+    const title = ref("hello");
     const myModealHide = (e: any) => {
       dialog.value = e;
     };
     return {
       dialog,
+      title,
       myModealHide,
     };
   },
