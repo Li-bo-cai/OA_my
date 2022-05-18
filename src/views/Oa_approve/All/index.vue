48d7860448e5902bf6f06d55d330b00e56ed3db2
libocai <1519497988@qq.com>
2022-05-18 18:00:15 +0800

    784

--- src/views/Oa_approve/All/index.vue
+++ src/views/Oa_approve/All/index.vue
@@ -1,52 +1,97 @@
 <template>
   <div>
+    <el-input label="输入框" v-model="form.values.input" @change="changeForm"></el-input>
+    {{form.values}}
     <FormProvider :form="form">
-      <SchemaField :schema="schema"></SchemaField>
+      <SchemaField :schema="schema" />
+      <Submit @submit="onSubmit">提交</Submit>
     </FormProvider>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 import { createForm } from "@formily/core";
 import { createSchemaField, FormProvider } from "@formily/vue";
-import { FormItem, Input } from "@formily/element-plus";
+import { FormItem, Input, Submit } from "@formily/element-plus";
+
+const { SchemaField } = createSchemaField({
+  components: {
+    Input,
+    FormItem,
+  },
+});
 
 export default defineComponent({
   components: {
     FormProvider,
-    // SchemaField
+    Submit,
+    SchemaField,
   },
   setup() {
-    const form = createForm();
-    const { SchemaField } = createSchemaField({
-      components: {
-        FormItem,
-        Input,
-      },
-    });
-    const schema = {
+    const form = reactive(
+      createForm({
+        values: {
+          input: "",
+        },
+      })
+    );
+
+    const schema = reactive({
       type: "object",
       properties: {
         input: {
           type: "string",
           title: "输入框",
           "x-decorator": "FormItem",
+          "x-decorator-props": {
+            label: "输入框",
+          },
           "x-component": "Input",
+          "x-component-props": {
+            placeholder: "请输入",
+          },
+          // "x-editable": true,
         },
         textarea: {
           type: "string",
           title: "文本框",
           "x-decorator": "FormItem",
+          "x-decorator-props": {
+            label: "文本框",
+          },
           "x-component": "Input.TextArea",
+          "x-component-props": {
+            type: "textarea",
+            placeholder: "请输入",
+            initialValues: "1111",
+          },
         },
       },
+    });
+
+    setTimeout(() => {
+      // schema.properties.input["x-decorator-props"].label = "4567";
+    }, 1000);
+
+    // setTimeout(() => {
+    //   form.values.input = "你好";
+    // }, 2000);
+
+    const changeForm = () => {
+      console.log(form);
+      console.log(schema);
+    };
+
+    const onSubmit = (value: any) => {
+      console.log(value);
     };
 
     return {
-      SchemaField,
       form,
       schema,
+      onSubmit,
+      changeForm,
     };
   },
 });
