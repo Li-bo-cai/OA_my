7329a544efd79c291112553963e6f00754d15eda
LbcNewBee <1519497988@qq.com>
2022-05-30 21:51:18 +0800

    456

--- src/views/Oa_approve/All/index.vue
+++ src/views/Oa_approve/All/index.vue
@@ -19,6 +19,27 @@ import { createSchemaField, FormProvider } from "@formily/vue";
 import { FormItem, Input, Submit, Form } from "@formily/element-plus";
 import { reactive } from "vue-demi";
 
+const form = createForm({
+  initialValues: {
+    input: "20",
+    input2: "",
+    textarea: "15",
+  },
+  effects() {
+    onFormReact((form) => {
+      console.log(form, "实现表单响应式逻辑");
+    });
+    onFormInputChange((form) => {
+      console.log(form, "监听字段输入");
+    });
+    onFormValuesChange((form) => {
+      console.log(form, "表单值变化");
+    });
+  },
+});
+const form2 = reactive({
+  input: "",
+});
 const schema = {
   type: "object",
   properties: {
@@ -55,35 +76,13 @@ const schema = {
   },
 };
 
-const form2 = reactive({
-  input: "",
-});
-
-const form = createForm({
-  initialValues: {
-    input: "20",
-    textarea: "15",
-  },
-  effects() {
-    onFormReact((form) => {
-      console.log(form, "实现表单响应式逻辑");
-    });
-    onFormInputChange((form) => {
-      console.log(form, "监听字段输入");
-    });
-    onFormValuesChange((form) => {
-      console.log(form, "表单值变化");
-    });
-  },
-});
-
 form.onMount = () => {
   console.log("我加载了");
   // form.subscribe((payload) => {
   //   console.log(payload, "---------------");
   //   form.setValues({ form: payload.payload.values });
   // });
-  form.setValues({ input: "20" });
+  // form.setValues({ input: "20" });
   // form.setState((state) => {
   //   console.log(12);
   // });
